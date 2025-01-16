c74c821e57b6717b24bd8dc1625458d01b9ec958
Augustine Shako <166209027+A-Shako@users.noreply.github.com>
2025-01-16 17:02:27 +0200

    Add files via upload

---
+++ Dial an artisan 1/Progressus/assets/js/google-map.js
@@ -0,0 +1,121 @@
+google.maps.event.addDomListener(window, 'load', init);
+
+var map;
+
+function init() {
+	var mapOptions = {
+		center: new google.maps.LatLng(-22.62890048820753, 17.0978785926267),
+		zoom: 17,
+		zoomControl: true,
+		zoomControlOptions: {
+			style: google.maps.ZoomControlStyle.SMALL,
+		},
+		disableDoubleClickZoom: true,
+		mapTypeControl: true,
+		mapTypeControlOptions: {
+			style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+		},
+		scaleControl: false,
+		scrollwheel: true,
+		streetViewControl: true,
+		draggable : true,
+		overviewMapControl: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		styles: [
+		{
+			featureType: "landscape",
+			stylers: [
+			{ saturation: -100 },
+			{ lightness: 65 },
+			{ visibility: "on" }
+			]
+		},{
+			featureType: "poi",
+			stylers: [
+			{ saturation: -100 },
+			{ lightness: 51 },
+			{ visibility: "simplified" }
+			]
+		},{
+			featureType: "road.highway",
+			stylers: [
+			{ saturation: -100 },
+			{ visibility: "simplified" }
+			]
+		},{
+			featureType: "road.arterial",
+			stylers: [
+			{ saturation: -100 },
+			{ lightness: 30 },
+			{ visibility: "on" }
+			]
+		},{
+			featureType: "road.local",
+			stylers: [
+			{ saturation: -100 },
+			{ lightness: 40 },
+			{ visibility: "on" }
+			]
+		},{
+			featureType: "transit",
+			stylers: [
+			{ saturation: -100 },
+			{ visibility: "simplified" }
+			]
+		},{
+			featureType: "administrative.province",
+			stylers: [
+			{ visibility: "off" }
+			]
+	/** /
+		},{
+			featureType: "administrative.locality",
+			stylers: [
+				{ visibility: "off" }
+			]
+		},{
+			featureType: "administrative.neighborhood",
+			stylers: [
+				{ visibility: "on" }
+			]
+			/**/
+		},{
+			featureType: "water",
+			elementType: "labels",
+			stylers: [
+			{ visibility: "on" },
+			{ lightness: -25 },
+			{ saturation: -100 }
+			]
+		},{
+			featureType: "water",
+			elementType: "geometry",
+			stylers: [
+			{ hue: "#ffff00" },
+			{ lightness: -25 },
+			{ saturation: -97 }
+			]
+		}
+		],
+		
+	}
+
+	var mapElement = document.getElementById('map');
+	var map = new google.maps.Map(mapElement, mapOptions);
+	var locations = [
+	
+	];
+
+	for (i = 0; i < locations.length; i++) {
+		marker = new google.maps.Marker({
+			icon: '',
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			map: map
+		});
+	}
+}
+<div id="map"></div>
+	<p><iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1193.9569239212951!2d17.09794120373279!3d-22.628930989046996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sna!4v1737026302113!5m2!1sen!2sna" width="1600" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></p>	<section class="container-full top-space">
+	
+	
+	</section>
